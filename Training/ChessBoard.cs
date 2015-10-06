81f965287b585ef919d5efdf0242ea81929f8649
Jose Mugaburu <jmugaburu@bulletline.com>
2015-10-06 17:06:21 -0400

    initializes first file for this training

---
+++ Training/ChessBoard.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    class ChessBoard
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;            
+            for (x = 0; x < 8; x++)
+            {
+                switch (x % 2 == 0)
+                {
+                    case true:
+                        GenerateLine("X");
+                        break;
+                    default:
+                        GenerateLine("O");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+
+
+
+
+        static void GenerateLine(string first)
+        {
+            string line = string.Empty;
+            string square;
+            int y = 0;
+            for (y = 0; y < 8; y++)
+            {
+                switch (y % 2 == 0)
+                {
+                    case true:
+                        square = "X";
+                        break;
+                    default:
+                        square = "O";
+                        break;
+                }
+
+                line += square;
+            }
+            Console.WriteLine(line);
+
+
+        }
+    }
+}
---
+++ Training/Student.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    class Student
+    {
+        //static void Main(string[] args)
+        //{
+
+        //    string firstName;
+        //    string lastName;
+        //    DateTime birthday = default(DateTime);
+        //    string fulladdr;
+        //    string addr1;
+        //    string addr2;
+        //    string city;
+        //    string state;
+        //    string zip;
+        //    string country;
+
+        //    firstName = lastName = addr1 = addr2 = city = state = zip = country = String.Empty;
+
+
+        //    Console.WriteLine("Please enter your First Name");
+        //    firstName = Console.ReadLine();
+            
+        //    Console.WriteLine("Please enter your Last Name");
+        //    lastName = Console.ReadLine();
+
+        //    Console.WriteLine("Please enter your birthday  (mm/dd/yyy)");
+        //    string day_input = Console.ReadLine();
+        //    //birthdayValidation(day_input);
+        //    birthday = Convert.ToDateTime(day_input);
+            
+        //    Console.WriteLine("Please enter your street address");
+        //    fulladdr = Console.ReadLine();
+
+        //    Console.WriteLine("Please enter your city");
+        //    city = Console.ReadLine();
+
+        //    Console.WriteLine("Please enter your State/Province");
+        //    state = Console.ReadLine();
+
+        //    Console.WriteLine("Please enter your Zip/Postal code");
+        //    zip = Console.ReadLine();
+
+        //    Console.WriteLine("Please enter your Country");
+        //    country = Console.ReadLine();
+
+        //    Console.WriteLine();
+        //    Console.WriteLine("Your Information");
+        //    Console.WriteLine("Full Name: " + firstName + " " + lastName);
+        //    Console.WriteLine("Birthday: " + day_input);
+        //    Console.WriteLine("Address: " + fulladdr);
+        //    Console.WriteLine("City: " + city);
+        //    Console.WriteLine("State: " + state);
+        //    Console.WriteLine("Zip/Postal:  " + zip);
+        //    Console.WriteLine("Country: " + country);
+        //    Console.ReadLine();
+
+
+
+        //}
+
+
+        //static bool birthdayValidation(string bday)
+        //{
+        //    return true;
+        //}
+
+    }
+}
